63c687556f2d7aa8b299944b7b84aeb0acd33852
wj-gu <fireman2634@naver.com>
2021-06-08 16:55:34 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '홍길동1',
+  'birthday' : '961222',
+  'gender' : '여자',
+  'job' : '대학생'
+  }, {
+    'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '홍길동2',
+  'birthday' : '961223',
+  'gender' : '남자',
+  'job' : '대학생'
+  }, {
+    'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '홍길동3',
+  'birthday' : '961224',
+  'gender' : '남자',
+  'job' : '대학생'
+  }
+]
+
 
 class App extends Component {
   render(){
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!</h2> 
-        <h3>Let's develop management system!!!</h3> 
-        <h3>Let's develop management system!!!</h3> 
-        <h3>Let's develop management system!!!</h3> 
-        <h3>Let's develop management system!!!</h3> 
-        <h3>Let's develop management system!!!</h3> 
-        <h3>Let's develop management system!!!</h3> 
-        <h3>Let's develop management system!!!</h3> 
-        <h3>Let's develop management system!!!</h3> 
-        <h3>Let's develop management system!!!</h3> 
-        <h3>Let's develop management system!!!</h3> 
-        <h3>Let's develop management system!!!</h3> 
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
       </div>
-    );
+      // <Customer
+      //   id={customer.id}
+      //   image={customer.image}
+      //   name={customer.name}
+      //   birthday={customer.birthday}
+      //   gender={customer.gender}
+      //   job={customer.job}
+      // />
+    ); 
   }
 }
 
