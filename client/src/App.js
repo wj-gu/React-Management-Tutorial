bac9918340c86c77f48737834b4b298f10c712f3
wj-gu <fireman2634@naver.com>
2021-06-15 10:02:44 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,35 +20,27 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '홍길동1',
-  'birthday' : '961222',
-  'gender' : '여자',
-  'job' : '대학생'
-  }, {
-    'id' : 2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '홍길동2',
-  'birthday' : '961223',
-  'gender' : '남자',
-  'job' : '대학생'
-  }, {
-    'id' : 3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '홍길동3',
-  'birthday' : '961224',
-  'gender' : '남자',
-  'job' : '대학생'
-  }
-]
-
-
 class App extends Component {
+
+  state = { // 변경할 수 있는 변수
+    customers: ""  
+  }
+
+  componentDidMount(){ // 모든 컴포넌트 준비가 완료되면 호출되는 함수
+     this.callApi()
+     .then(res => this.setState({customers: res}))
+     .catch(err => console.log(err));
+     
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render(){
-    const {classes} = this.props;
+    const {classes} = this.props; // props는 변경할 수 없는 변수
     return (
       <Paper className={classes.root}>
         <Table className={classes.table}>
@@ -64,7 +56,7 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {
-              customers.map(c => {
+              this.state.customers ? this.state.customers.map(c => {
                 return (
                   <Customer
                     key={c.id}
@@ -76,7 +68,7 @@ class App extends Component {
                     job={c.job}
                   />
                 );
-              })
+              }) : ""
             }
           </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -3,11 +3,34 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // json 형태로 변환해 주는 역할을 함.
 app.use(bodyParser.urlencoded({ extenede: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message : 'Hello Express!'});
-});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+        'id' : 1,
+        'image' : 'https://placeimg.com/64/64/1',
+        'name' : '홍길동1',
+        'birthday' : '961222',
+        'gender' : '여자',
+        'job' : '대학생'
+        }, {
+            'id' : 2,
+        'image' : 'https://placeimg.com/64/64/2',
+        'name' : '홍길동2',
+        'birthday' : '961223',
+        'gender' : '남자',
+        'job' : '대학생'
+        }, {
+            'id' : 3,
+        'image' : 'https://placeimg.com/64/64/3',
+        'name' : '홍길동3',
+        'birthday' : '961224',
+        'gender' : '남자',
+        'job' : '대학생'
+        }
+    ]);
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
